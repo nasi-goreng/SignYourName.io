18b0ea71ac7a6c637c746d2e32eace79db383b72
nasi-goreng <54302736+nasi-goreng@users.noreply.github.com>
2024-04-30 18:48:07 +0900

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import WebcamFeed from './WebcamFeed.jsx';
 import { ReactComponent as CheckMark } from './assets/checkmark.svg';
+import Header from './Header.jsx';
 
 function App() {
   const [name, setName] = useState('');
@@ -29,26 +30,26 @@ function App() {
   };
 
   return (
-    <div className="App flex flex-col items-center">
-      <h1 className="text-4xl mb-4">SignYourName.io</h1>
-      <input 
-        type="text" 
-        value={name} 
-        onChange={handleNameChange} 
-        autoFocus 
-        className="mb-4 p-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500" 
-      />
-      <WebcamFeed className="mb-4 w-full md:w-1/2" />
-      <div id="sign-images" className="flex flex-wrap justify-center">
-        {name.split('').map((letter, index) => (
-      <div className="relative m-2">
-        <img key={index} src={`/images/${letter}.jpg`} alt={letter} className="w-full h-full" />
-        {successfulGestures[index] && <CheckMark className="absolute inset-0 w-full h-full text-green-500" />} {/* Conditionally render check mark */}
+    <>
+      <Header />
+      <div className="App flex flex-col items-center">
+        {/* <h1 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">SignYourName.io</h1> */}
+        <div class="m-4">
+        <input type="text" value={name} placeholder="your name" onChange={handleNameChange}  class="bg-transparent py-1 border-2 border-gray-300 rounded-md text-gray-600 px-4 focus:outline-none" />
+        </div>
+        <WebcamFeed className="mb-4 w-full md:w-1/2" />
+        <div id="sign-images" className="flex flex-wrap justify-center">
+          {name.split('').map((letter, index) => (
+        <div className="relative m-2">
+          <img key={index} src={`/images/${letter}.jpg`} alt={letter} className="w-full h-full" />
+          {successfulGestures[index] && <CheckMark className="absolute inset-0 w-full h-full text-green-500" />} {/* Conditionally render check mark */}
+          {/* https://commons.wikimedia.org/wiki/File:Eo_circle_green_checkmark.svg */}
+        </div>
+          ))}
+        </div>
+        <button onClick={() => handleGestureSuccess('A')} className="mb-4 p-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500">Simulate 'A' Gesture</button>
       </div>
-        ))}
-      </div>
-      <button onClick={() => handleGestureSuccess('A')} className="mb-4 p-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500">Simulate 'A' Gesture</button>
-    </div>
+    </>
   );
 }
 
