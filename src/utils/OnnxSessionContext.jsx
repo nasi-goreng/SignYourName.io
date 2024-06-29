f73bc4c3195f6c1715862bcb76f8bf63f1bd8351
nasi-goreng <54302736+nasi-goreng@users.noreply.github.com>
2024-06-29 19:34:48 +0900

    changed onnx model loading timing

--- src/utils/OnnxSessionContext.jsx
+++ src/utils/OnnxSessionContext.jsx
@@ -1,5 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import * as ort from 'onnxruntime-web';
+import { ONNX } from '../modelConfigs';
 
 const OnnxSessionContext = createContext(null);
 
@@ -11,16 +12,18 @@ export const OnnxSessionProvider = ({ children, modelConfig }) => {
   const [session, setSession] = useState(null);
 
   useEffect(() => {
-    const loadModel = async () => {
-      try {
-        const session = await ort.InferenceSession.create(modelConfig.path);
-        setSession(session);
-      } catch (err) {
-        console.error('Failed to load ONNX model:', err);
-      } 
-    };
+    if (modelConfig.modelExportType === ONNX) {
+      const loadModel = async () => {
+        try {
+          const session = await ort.InferenceSession.create(modelConfig.path);
+          setSession(session);
+        } catch (err) {
+          console.error('Failed to load ONNX model:', err);
+        }
+      };
 
-    loadModel();
+      loadModel();
+    }
   }, [modelConfig.path]);
 
 
