5dffeffd6c30f9591895ed365cef65f1595efe71
nasi-goreng <54302736+nasi-goreng@users.noreply.github.com>
2024-07-01 22:04:39 +0900

    changed size

--- src/components/MobileLandingPage.jsx
+++ src/components/MobileLandingPage.jsx
@@ -17,19 +17,18 @@ const MobileLandingPage = () => {
 
   return (
     <div className="min-h-screen bg-[#8953CD] flex flex-col items-center justify-center text-white space-y-10">
-      <h1 className="text-6xl font-semibold relative text-white">
+      <h1 className="text-4xl font-semibold relative text-white">
         Sign<i>Your </i>Name
       </h1>
-      <div className="w-[392px] h-[538px] flex items-center justify-center z-10">
+      <div className="w-[235.2px] h-[322.8px] flex items-center justify-center z-10">
         <img src={imagePath + currentImage + '.png'} alt="Hand Illustration" className="h-full" />
       </div>
-      <p className="mt-6 text-xl leading-20 text-center dm-mono">
+      <p className="mt-6 text-base leading-20 text-center dm-mono">
         Thanks for checking our work out!
         <br />
         Our site is not optimized for mobile devices :(
         <br />
         Please visit us again on from your desktop
-        <br />
         to checkout the project!
       </p>
     </div>
